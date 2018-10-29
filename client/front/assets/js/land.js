8cb97f66350f2a6dea2846dc8be6e9082fd7e1cc
csr <2548772327@qq.com>
2018-10-29 20:27:22 +0800

    登录表单验证

--- client/front/assets/js/land.js
+++ client/front/assets/js/land.js
@@ -1,11 +1,41 @@
 new Vue({
     el: '#land',
     delimiters: ['${', '}'],
-    data: {
-        stuNo: "",
-        stuName: ""
+    data() {
+        return {
+            ruleForm: {
+                stuNo: "",
+                stuName: ""
+            },
+            rules: {
+                stuNo: [
+                    {required: true, message: '请输入学号', trigger: 'blur'},
+                    {min: 10, max: 11, message: '长度在10个字符', trigger: 'blur'},
+                ],
+                stuName: [
+                    {required: true, message: '请输入姓名', trigger: 'blur'},
+                    {min: 2, max: 5, message: '长度在2到5个字符', trigger: 'blur'},
+                ],
+            }
+
+        }
     },
     methods: {
+        submitForm(formName) {
+            var that = this;
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    that.land();
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+        },
+
         clearInput: function () {
             this.stuNo = "";
             this.stuName = "";
@@ -13,10 +43,9 @@ new Vue({
         land: function () {
             that = this;
             var data = {
-                stuNo: that.stuNo,
-                stuName: that.stuName
+                stuNo: that.ruleForm.stuNo,
+                stuName: that.ruleForm.stuName
             }
-            console.log(data);
             $.ajax({
                 type: 'Post',
                 url: FRONT_URL + "/auth/login",
@@ -25,7 +54,10 @@ new Vue({
                 headers: {'Content-Type': 'application/json'},
                 success: function (resData) {
                     if (resData.retCode === API_SUCCEED_CODE) {
-location.replace('/land')
+                        location.replace('/front/list')
+                    }
+                    else {
+
                     }
                 },
                 error: function (xhr, errorType, error) {
@@ -35,5 +67,6 @@ location.replace('/land')
 
             })
         }
+
     }
 });
